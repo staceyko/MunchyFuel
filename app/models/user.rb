9f369d2347684e8bd6361ca8b22731738bb6933a
George Toothman <george.toothman@flatironschool.com>
2015-09-21 15:05:04 -0400

    add branch to test geocoder

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,25 @@
+# require 'rest-client'
+# require 'geocoder'
+
 class User < ActiveRecord::Base
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+#   # Include default devise modules. Others available are:
+#   # :confirmable, :lockable, :timeoutable and :omniauthable
+#   devise :database_authenticatable, :registerable,
+#          :recoverable, :rememberable, :trackable, :validatable
 
-  geocoded_by :ip_address,
-  :latitude => :lat, :longitude => :lon
-  after_validation :geocode
+#   geocoded_by :ip_address,
+#   :latitude => :lat, :longitude => :lon
+#   after_validation :geocode
 
+  # def get_user_location
+  #   ip = RestClient.get("http://echoip.net/")
+  #   results = Geocoder.search(ip)[0]
+  #   latitude = results.latitude
+  #   longitude = results.longitude
+  #   return latitude, longitude
+  # end
 end
+
+# test = User.new
+# puts test.get_user_location
