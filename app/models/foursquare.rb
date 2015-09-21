cef60ef1a254294a6bb4092bdabce820fc988ac6
George Toothman <george.toothman@flatironschool.com>
2015-09-21 17:21:13 -0400

    add index directory with welcome template, worked on foursquare model logic for rendering photos

--- app/models/foursquare.rb
+++ app/models/foursquare.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Foursquare < ActiveRecord::Base
   include FoodsHelper
   attr_reader :pic_url, :phone_num, :address
@@ -7,44 +6,45 @@ class Foursquare < ActiveRecord::Base
     self.search_venues
     self.pick_random_venue
     self.get_pictures
-    @pic_url = self.pick_random_picture
-    @phone_num = self.get_phone_num
-    @address = self.get_address
+    self.pick_random_picture
+    self.get_phone_num
+    self.get_address
   end
 
   def search_venues
-    random_food = return_food_array.sample
-
-    result = @client.search_venues(:ll => '36.142064,-86.816086', :query => random_food)
-    @venues = result["venues"]
-    binding.pry
+    # until @venues != []
+      @random_food = return_food_array.sample
+      result = @client.search_venues(:ll => '40.7048872,-74.0123737', :query => @random_food)
+      @venues = result["venues"]
+    # end
   end
 
   def pick_random_venue
-    @stored_venue = @venues.sample
+    # until get_pictures != []
+      @stored_venue = @venues.sample
+    # end
   end
 
   def get_pictures
     photo_result = @client.venue_photos(@stored_venue["id"])
-    @photos = photo_result["items"]
   end
 
   def pick_random_picture
-    @stored_photo = @photos.sample
+    @stored_photo = get_pictures["items"].sample
 
     photo_id = @stored_photo["id"]
     prefix = @stored_photo["prefix"]
     width = @stored_photo["width"]
     height = @stored_photo["height"]
     suffix = @stored_photo["suffix"]
-    photo_url = "#{prefix}#{width}x#{height}#{suffix}"
+    @pic_url = "#{prefix}#{width}x#{height}#{suffix}"
   end
 
   def get_phone_num
-    @stored_venue["contact"]["formattedPhone"]
+    @phone_num = @stored_venue["contact"]["formattedPhone"]
   end
 
   def get_address
-    @stored_venue["location"]["formattedAddress"]
+    @address = @stored_venue["location"]["formattedAddress"]
   end
 end
