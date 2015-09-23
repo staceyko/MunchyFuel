95748c7135318a5e92de5bd00926dc345f2c3aa1
staceyko <ko.stacey@gmail.com>
2015-09-23 10:46:59 -0400

    add migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922150501) do
+ActiveRecord::Schema.define(version: 20150923144315) do
 
   create_table "dislikes", force: :cascade do |t|
-    t.integer "foursquares_id"
+    t.integer "foursquare_id"
   end
 
   create_table "foursquares", force: :cascade do |t|
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "vote"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "foursquares_id"
+    t.integer "foursquare_id"
   end
 
   create_table "users", force: :cascade do |t|
