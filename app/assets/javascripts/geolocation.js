e479ed9086064740eb5cfc0aa8eabf43b6f2936e
sherriously <lusherri@gmail.com>
2015-09-25 14:35:20 -0400

    add user functinoality

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,17 +1,17 @@
+function getGeoLocation() {
+  navigator.geolocation.getCurrentPosition(setGeoCookie);
+}
+
+function setGeoCookie(position) {
+  var cookie_val = position.coords.latitude + "|" + position.coords.longitude;
+  document.cookie = "lat_lng=" + escape(cookie_val);
+}
+
+function deleteCookie(name) {
+    document.cookie = name + '=; Max-Age=0';
+}
+
 $(document).ready(function() {
-  function getGeoLocation() {
-    navigator.geolocation.getCurrentPosition(setGeoCookie);
-  }
-
-  function setGeoCookie(position) {
-    var cookie_val = position.coords.latitude + "|" + position.coords.longitude;
-    document.cookie = "lat_lng=" + escape(cookie_val);
-  }
-
-  function deleteCookie(name) {
-      document.cookie = name + '=; Max-Age=0';
-  }
-
   $('#location').click(function() {
     deleteCookie("lat_lng=");
     getGeoLocation();
