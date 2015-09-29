f2e08a33b7ebce097e6c7ff695d40a2f4c7ccb86
George Toothman <george.toothman@flatironschool.com>
2015-09-29 08:21:45 -0400

    rename app/assets/stylesheets/application.scss to .css.erb

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,9 @@ module MunchyFuel
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    
+    # accomplishes same thing as requiring rails_12factor in gemfile
+    config.serve_static_assets = true
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
